445e84de5a8c24ff42009e93b3f5abf21a26d26e
Chandra <dcsekars_2000@yahoo.com>
2023-07-25 12:20:07 +0530

    myfirst commit

---
+++ terraform.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = "~> 1.5.2"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "chandradc-general"
+  region  = "us-east-1"
+}
